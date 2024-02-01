19db131fe71c11973242d6858c3d585fa44ad1aa
Taylor-Brandon <117056023+Taylor-Brandon@users.noreply.github.com>
2024-02-01 14:09:51 -0500

    added index.js, app.js, and sign.js code and tested

---
+++ solas-boat-app/src/app.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import Sign from './components/sign';
+
+ function App() {
+  return <Sign />;
+}
+
+export default App;
---
+++ solas-boat-app/src/components/sign.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+function Sign() {
+    return(
+        <h1>Hello</h1>
+    );
+}
+
+export default Sign;
