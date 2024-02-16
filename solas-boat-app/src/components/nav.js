6ef32906a2326d01ea0b6cfc5817e769fe1b7600
Taylor-Brandon <117056023+Taylor-Brandon@users.noreply.github.com>
2024-02-15 19:16:56 -0500

    moved admin role and username to nav

--- solas-boat-app/src/components/nav.js
+++ solas-boat-app/src/components/nav.js
@@ -1,48 +1,69 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React from "react";
+import { Link } from "react-router-dom";
 import "../styles/nav.css";
 
-function Navigation() {
-    return (
-        <div className='navbar'>
-        <button id="toggle-btn" className="navbar-toggler bg-warning p-2" 
+function Navigation({ userNameInput, isAdminString }) {
+  console.log(userNameInput);
+  return (
+    <div className="navbar">
+      <button
+        id="toggle-btn"
+        className="navbar-toggler bg-warning p-2"
+        type="button"
+        data-bs-toggle="offcanvas"
+        data-bs-target="#offcanvasDarkNavbar"
+        aria-controls="offcanvasDarkNavbar"
+        aria-label="Toggle navigation"
+      >
+        <span className="navbar-toggler-icon"></span>
+      </button>
+      <div
+        className="offcanvas offcanvas-end text-bg-dark"
+        tabIndex="-1"
+        id="offcanvasDarkNavbar"
+        aria-labelledby="offcanvasDarkNavbarLabel"
+      >
+        <div className="offcanvas-header">
+          <h5 className="offcanvas-title" id="offcanvasDarkNavbarLabel">
+            Navigation
+          </h5>
+          <h5 id="userNameInput">Username: {userNameInput}</h5>
+          <h5 id="adminRole">Role: {isAdminString}</h5>
+          <button
             type="button"
-            data-bs-toggle="offcanvas"
-            data-bs-target="#offcanvasDarkNavbar"
-            aria-controls="offcanvasDarkNavbar"
-            aria-label="Toggle navigation">
-            <span className="navbar-toggler-icon"></span>
-        </button>
-        <div className="offcanvas offcanvas-end text-bg-dark" tabIndex="-1" id="offcanvasDarkNavbar" aria-labelledby="offcanvasDarkNavbarLabel">
-            <div className="offcanvas-header">
-                <h5 className="offcanvas-title" id="offcanvasDarkNavbarLabel">Navigation</h5>
-                <button type="button" className="btn-close btn-close-white" data-bs-dismiss="offcanvas" aria-label="Close"></button>
-            </div>
-            <div className="offcanvas-body">
-                <ul className="navbar-nav justify-content-end flex-grow-1 pe-3">
-                    <li className="nav-item">
-                        <Link id='homeLink' className='searchLink' to='/home'>Home</Link>
-                    </li>
-                    <li className="nav-item">
-                        <Link id='pdfLink' className='pdfLink' to='/pdf'>PDF</Link>
-                    </li>
-                    <li>
-                        <button className='btn btn-warning'>Add Data</button>
-                    </li>
-                    <li>
-                        <button className='btn btn-warning'>Adjust Roles</button>
-                    </li>
-                    <li>
-                    <button id='password' className='btn btn-warning'>Change Password</button>
-                    </li>
-                </ul>
-            </div>
+            className="btn-close btn-close-white"
+            data-bs-dismiss="offcanvas"
+            aria-label="Close"
+          ></button>
         </div>
+        <div className="offcanvas-body">
+          <ul className="navbar-nav justify-content-end flex-grow-1 pe-3">
+            <li className="nav-item">
+              <Link id="homeLink" className="searchLink" to="/home">
+                Home
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link id="pdfLink" className="pdfLink" to="/pdf">
+                PDF
+              </Link>
+            </li>
+            <li>
+              <button className="btn btn-warning">Add Data</button>
+            </li>
+            <li>
+              <button className="btn btn-warning">Adjust Roles</button>
+            </li>
+            <li>
+              <button id="password" className="btn btn-warning">
+                Change Password
+              </button>
+            </li>
+          </ul>
+        </div>
+      </div>
     </div>
-    
-
-    )
+  );
 }
 
 export default Navigation;
-
--- solas-boat-app/src/components/profile.js
+++ solas-boat-app/src/components/profile.js
@@ -1,41 +1,47 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
-import Navigation from '../components/nav';
-import '../styles/profile.css';
-import 'bootstrap/dist/css/bootstrap.min.css';  
+import React from "react";
+import { Link } from "react-router-dom";
+import Navigation from "../components/nav";
+import "../styles/profile.css";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 function Profile({ firstName, lastName, isAdmin, userNameInput }) {
-    console.log(isAdmin);
-    console.log(userNameInput);
-    const isAdminString = isAdmin ? "Admin" : "Not Admin";
+  console.log(isAdmin);
 
-    return (
-        <div className='container'>
-            {!isAdmin ? (
-                <>
-                    <h1 id='admin'>Hello: {isAdminString}</h1>
-                    <h1 id="user">{userNameInput}</h1>
-                    <div className='bg-image'>
-                        <div className='bd-text'></div>
-                    </div>
-                    <h1 className='welcomeProfile'>Welcome {firstName} {lastName}</h1>
-                    <Link className='logOut' to='/'>Logout</Link>
-                    
-                </>
-            ) : (
-                <>
-                    <h1 id="admin">Hello: {isAdminString}</h1>
-                    <h1 id="user">{userNameInput}</h1>
-                    <div className='bg-image'>
-                        <div className='bg-text'></div> 
-                    </div>
-                    <h1 className='welcomeProfile'>Welcome {firstName} {lastName}</h1>
-                    <Link className='logOut' to='/'>Logout</Link>
-                    <Navigation />
-                </>
-            )}
-        </div>
-    );
+  const isAdminString = isAdmin ? "Admin" : "Not Admin";
+
+  return (
+    <div className="container">
+      {!isAdmin ? (
+        <>
+          <div className="bg-image">
+            <div className="bd-text"></div>
+          </div>
+          <h1 className="welcomeProfile">
+            Welcome {firstName} {lastName}
+          </h1>
+          <Link className="logOut" to="/">
+            Logout
+          </Link>
+        </>
+      ) : (
+        <>
+          <div className="bg-image">
+            <div className="bg-text"></div>
+          </div>
+          <h1 className="welcomeProfile">
+            Welcome {firstName} {lastName}
+          </h1>
+          <Link className="logOut" to="/">
+            Logout
+          </Link>
+          <Navigation
+            isAdminString={isAdminString}
+            userNameInput={userNameInput}
+          />
+        </>
+      )}
+    </div>
+  );
 }
 
 export default Profile;
