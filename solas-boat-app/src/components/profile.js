70a7add3c8c3092281b36853948c64f61d0053bf
Taylor-Brandon <117056023+Taylor-Brandon@users.noreply.github.com>
2024-02-15 19:19:06 -0500

    added logout logo to the profile component

--- solas-boat-app/src/components/profile.js
+++ solas-boat-app/src/components/profile.js
@@ -2,6 +2,8 @@ import React from "react";
 import { Link } from "react-router-dom";
 import Navigation from "../components/nav";
 import "../styles/profile.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faRightFromBracket } from "@fortawesome/free-solid-svg-icons";
 import "bootstrap/dist/css/bootstrap.min.css";
 
 function Profile({ firstName, lastName, isAdmin, userNameInput }) {
@@ -32,7 +34,7 @@ function Profile({ firstName, lastName, isAdmin, userNameInput }) {
             Welcome {firstName} {lastName}
           </h1>
           <Link className="logOut" to="/">
-            Logout
+            <FontAwesomeIcon icon={faRightFromBracket} size="2x" />
           </Link>
           <Navigation
             isAdminString={isAdminString}
