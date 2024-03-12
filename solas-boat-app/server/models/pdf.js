bcd6ea9658f8ca4ac77d96c0ad837e62cbb993ea
Taylor-Brandon <117056023+Taylor-Brandon@users.noreply.github.com>
2024-03-12 19:57:44 -0400

    added all server files and content

---
+++ solas-boat-app/server/models/pdf.js
@@ -0,0 +1,16 @@
+const { Schema, model } = require('mongoose');
+
+const pdfSchema = new Schema({
+  fileName: {
+    type: String,
+    required: true
+  },
+  path: {
+    type: String,
+    required: true
+  },
+});
+
+const Pdf = model('Pdf', pdfSchema);
+
+module.exports = Pdf;
---
+++ solas-boat-app/server/schemas/typeDefs.js
@@ -0,0 +1,42 @@
+const { gql } = require("apollo-server-express");
+
+const typeDefs = gql`
+  type User {
+    _id: ID
+    firstName: String
+    lastName: String
+    userName: String
+    password: String
+    admin: Boolean
+  }
+  type Ship {
+    _id: ID
+    Ship: String
+    Model: String
+    HRN: String
+    HIN: String
+    contactNumber: String
+    sponsonSerialNumber: String
+    SRBSerialNumber: String
+    fuelTankSerialNumber: String
+    ZAPR356C2BVMXHookSerialNumber: String
+    engineMakeModel: String
+    engineSerialNumber: String
+    POCName: String
+    POCEmail: String
+    POCPhoneNumber: String
+    Notes: String
+  }
+  type Pdf {
+    _id: ID
+    fileName: String
+    path: String
+  }
+  type Query {
+    users: [User]
+    ships: [Ship]
+    pdfs: [Pdf]
+  }
+`;
+
+module.exports = typeDefs;
