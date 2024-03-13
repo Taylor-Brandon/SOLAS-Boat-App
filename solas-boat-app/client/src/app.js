afa0e11815aee18d858fef73c558e4750e116e1f
Taylor-Brandon <117056023+Taylor-Brandon@users.noreply.github.com>
2024-03-13 14:08:54 -0400

    added app.js logic

--- solas-boat-app/client/src/app.js
+++ solas-boat-app/client/src/app.js
@@ -1,11 +1,38 @@
 import React, { useState } from 'react';
+import {
+  ApolloClient,
+  InMemoryCache,
+  ApolloProvider,
+  createHttpLink,
+} from '@apollo/client';
+import { setContext } from '@apollo/client/link/context';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+
 import Sign from './components/sign';
 import Home from './components/home';
 import Log from './components/log';
 import Results from './components/results';
 import Profile from './components/profile';
 import PDFViewer from './components/pdf';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+
+const httpLink = createHttpLink({
+  uri: '/graphql',
+});
+
+const authLink = setContext((_, { headers }) => {
+  const token = localStorage.getItem('id_token');
+  return {
+    headers: {
+      ...headers,
+      authorization: token ? `Bearer ${token}` : '',
+    },
+  };
+});
+
+const client = new ApolloClient({
+  link: authLink.concat(httpLink),
+  cache: new InMemoryCache(),
+});
 
 function App() {
   const [firstName, setFirstName] = useState('');
@@ -15,6 +42,7 @@ function App() {
   const [isAdmin, setIsAdmin] = useState(false);
 
   return (
+    <ApolloProvider client={client}>
     <Router>
       <Routes>
       <Route path="/" element={<Sign setLoggedIn={setLoggedIn} setFirstName={setFirstName} setLastName={setLastName} setIsAdmin={setIsAdmin} setUserNameInput={setUserNameInput} />} />
@@ -26,6 +54,7 @@ function App() {
         <Route path="/pdf" element={<PDFViewer />} />
       </Routes>
     </Router>
+    </ApolloProvider>
   );
 }
 
--- solas-boat-app/client/src/index.js
+++ solas-boat-app/client/src/index.js
@@ -1,7 +1,8 @@
 import React from 'react'; 
 import ReactDOM from 'react-dom'; 
 import App from './app'; 
-import 'bootstrap/dist/css/bootstrap.min.css';  
+import 'bootstrap/dist/css/bootstrap.min.css'; 
+import reportWebVitals from './reportWebVitals';
 
 
 
@@ -12,3 +13,4 @@ ReactDOM.render(
     </React.StrictMode>,
     document.getElementById('root')
   );
+  reportWebVitals();
