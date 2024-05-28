daae6571933ab24fa25466a5ba40f4e5729022a1
Taylor-Brandon <117056023+Taylor-Brandon@users.noreply.github.com>
2024-05-28 13:31:37 -0400

    added five year inspection to search

--- solas-boat-app/client/src/components/search/search.js
+++ solas-boat-app/client/src/components/search/search.js
@@ -26,7 +26,8 @@ const Search = () => {
       ship.Model.toLowerCase().includes(searchQuery.toLowerCase())||
       ship.HRN.toLowerCase().includes(searchQuery.toLowerCase()) ||
       ship.HIN.toLowerCase().includes(searchQuery.toLowerCase())||
-      ship.annualInspectionDate.toLowerCase().includes(searchQuery.toLowerCase()) 
+      ship.annualInspectionDate.toLowerCase().includes(searchQuery.toLowerCase()) ||
+      ship.fiveYearInspectionDate.toLowerCase().includes(searchQuery.toLowerCase())
     );
     setFilteredShips(filteredShips);
   };
